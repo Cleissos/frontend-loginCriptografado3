b30cfc161adc3cb1bc654ee85910625ddc16af5a
Cleisson <mc4220762@gmail.com>
2026-02-14 00:54:19 -0300

    Mudança de CREATE-REACT-APP para VITE

--- src/services/api.js
+++ src/services/api.js
@@ -39,7 +39,9 @@
 //     return response;
 // }
 
-const API_URL = process.env.REACT_APP_API_URL;
+// const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
+
 
 export const cadastrarUsuario = async (userData) => {
     const response = await fetch(`${API_URL}/register`, {
